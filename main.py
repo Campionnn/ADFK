00536535836cf5e37ee3ebe0792435e630fc46e8
Campion <ryanchien2468@gmail.com>
2024-06-30 02:24:55 -0700

    changing to int broke things

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
-import ctypes
+import os
 import struct
 import time
 from control import Control
+from PyMemoryEditor import OpenProcess
 
-from PyMemoryEditor import OpenProcess, ScanTypesEnum
-
-y_addrs = 0x1ab87ef4854
-x_addrs = y_addrs - 0x4
-z_addrs = y_addrs + 0x4
-pitch_addrs = y_addrs + 0x18C
-yaw_addrs = y_addrs + 0x188
-
-addrs = [x_addrs, y_addrs, z_addrs, pitch_addrs, yaw_addrs]
+try:
+    import memory_search
+except ImportError:
+    os.system("python setup.py build_ext --inplace")
+    import memory_search
 
 process_name = "RobloxPlayerBeta.exe"
 with OpenProcess(process_name=process_name) as process:
-    print(f"Process ID: {process.pid}")
-    while True:
-        for address, value in process.search_by_addresses(bytes, 4, addrs):
-            if address == x_addrs:
-                print(f"x: {struct.unpack('f', value)[0]}")
-            elif address == y_addrs:
-                print(f"y: {struct.unpack('f', value)[0]}")
-            elif address == z_addrs:
-                print(f"z: {struct.unpack('f', value)[0]}")
-            elif address == pitch_addrs:
-                print(f"pitch: {struct.unpack('f', value)[0]}")
-            elif address == yaw_addrs:
-                print(f"yaw: {struct.unpack('f', value)[0]}")
-        time.sleep(1)
+    print(process.pid)
+    # addresses = memory_search.search_memory_for_float(process.pid, 12.680, 0.0005)
+    # for address in addresses:
+    #     print(f"Found address: {address}")
+    # print(len(addresses))
+    # input("Press enter once in afk...")
+    # addresses2 = memory_search.search_memory_for_float_in_addresses(process.pid, addresses, -2.3425, 0.005)
+    # for address in addresses2:
+    #     print(f"Found address: {address}")
+    # y_addrs = 0x2E5E72195E4
+    # x_addrs = y_addrs - 0x4
+    # z_addrs = y_addrs + 0x4
+    # pitch_addrs = y_addrs + 0x18C
+    # yaw_addrs = y_addrs + 0x188
+    # addrs = [x_addrs, y_addrs, z_addrs, pitch_addrs, yaw_addrs]
+    # print(f"Process ID: {process.pid}")
+    # while True:
+    #     for address, value in process.search_by_addresses(bytes, 4, addrs):
+    #         if address == x_addrs:
+    #             print(f"x: {struct.unpack('f', value)[0]}")
+    #         elif address == y_addrs:
+    #             print(f"y: {struct.unpack('f', value)[0]}")
+    #         elif address == z_addrs:
+    #             print(f"z: {struct.unpack('f', value)[0]}")
+    #         elif address == pitch_addrs:
+    #             print(f"pitch: {struct.unpack('f', value)[0]}")
+    #         elif address == yaw_addrs:
+    #             print(f"yaw: {struct.unpack('f', value)[0]}")
+    #     time.sleep(1)
--- memory_search.cpp
+++ memory_search.cpp
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include <vector>
 #include <algorithm>
+#include <cstdint>
 
 bool ReadMemory(HANDLE hProcess, LPCVOID address, LPVOID buffer, SIZE_T size) {
     SIZE_T bytesRead;
@@ -9,9 +10,13 @@ bool ReadMemory(HANDLE hProcess, LPCVOID address, LPVOID buffer, SIZE_T size) {
 }
 
 #undef min
-std::vector<LPCVOID> SearchMemoryForFloat(DWORD pid, float targetValue, float tolerance) {
-    std::vector<LPCVOID> foundAddresses;
-    HANDLE hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, FALSE, pid);
+std::vector<int> SearchMemoryForFloat(DWORD pid, float targetValue, float tolerance) {
+    std::vector<int> foundAddresses;
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+    if (hProcess == NULL) {
+        foundAddresses.push_back(-1);
+        return foundAddresses;
+    }
 
     SYSTEM_INFO sysInfo;
     GetSystemInfo(&sysInfo);
@@ -19,15 +24,16 @@ std::vector<LPCVOID> SearchMemoryForFloat(DWORD pid, float targetValue, float to
     LPCVOID endAddress = sysInfo.lpMaximumApplicationAddress;
 
     MEMORY_BASIC_INFORMATION memInfo;
-    std::vector<char> buffer(4096);
+    int bufferSize = 0x400;
+    std::vector<char> buffer(bufferSize);
 
     for (LPCVOID address = startAddress; address < endAddress;) {
         if (VirtualQueryEx(hProcess, address, &memInfo, sizeof(memInfo)) == 0) {
-            address = reinterpret_cast<LPCVOID>(reinterpret_cast<SIZE_T>(address) + 0x1000);
+            address = reinterpret_cast<LPCVOID>(reinterpret_cast<SIZE_T>(address) + bufferSize);
             continue;
         }
 
-        if (memInfo.State == MEM_COMMIT && (memInfo.Protect == PAGE_READWRITE || memInfo.Protect == PAGE_READONLY)) {
+        if (memInfo.State == MEM_COMMIT && (memInfo.Protect == PAGE_READWRITE || memInfo.Protect == PAGE_READONLY || memInfo.Protect == PAGE_WRITECOPY)) {
             SIZE_T regionSize = memInfo.RegionSize;
             LPCVOID regionBase = memInfo.BaseAddress;
 
@@ -40,9 +46,12 @@ std::vector<LPCVOID> SearchMemoryForFloat(DWORD pid, float targetValue, float to
                     for (SIZE_T i = 0; i < numFloats; ++i) {
                         float value = floatBuffer[i];
                         if (std::abs(value - targetValue) <= tolerance) {
-                            foundAddresses.push_back(reinterpret_cast<LPCVOID>(reinterpret_cast<SIZE_T>(regionBase) + bytesRead + i * sizeof(float)));
+                            foundAddresses.push_back(static_cast<int>(reinterpret_cast<SIZE_T>(regionBase) + bytesRead + i * sizeof(float)));
+                            std::cout << "Found: " << value << " at address: " << std::hex << foundAddresses.back() << std::endl;
                         }
                     }
+                } else {
+                    break;
                 }
                 bytesRead += bytesToRead;
             }
@@ -55,13 +64,20 @@ std::vector<LPCVOID> SearchMemoryForFloat(DWORD pid, float targetValue, float to
     return foundAddresses;
 }
 
-std::vector<LPCVOID> SearchMemoryForFloatInAddresses(DWORD pid, std::vector<LPCVOID> addresses, float targetValue, float tolerance) {
-    std::vector<LPCVOID> foundAddresses;
-    HANDLE hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, FALSE, pid);
+std::vector<int> SearchMemoryForFloatInAddresses(DWORD pid, std::vector<int> addresses, float targetValue, float tolerance) {
+    std::vector<int> foundAddresses;
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+    if (hProcess == NULL) {
+        foundAddresses.push_back(-1);
+        return foundAddresses;
+    }
 
     float buffer;
     for (auto address : addresses) {
-        if (ReadMemory(hProcess, address, &buffer, sizeof(float))) {
+        LPCVOID addressLPCVOID = reinterpret_cast<LPCVOID>(reinterpret_cast<SIZE_T>(static_cast<uintptr_t>(static_cast<uint32_t>(address))));
+        std::cout << "Checking address: " << addressLPCVOID << std::endl;
+        if (ReadMemory(hProcess, addressLPCVOID, &buffer, sizeof(float))) {
+            std::cout << "Checking: " << buffer << " at address: " << std::hex << address << std::endl;
             if (std::abs(buffer - targetValue) <= tolerance) {
                 foundAddresses.push_back(address);
             }
@@ -72,12 +88,35 @@ std::vector<LPCVOID> SearchMemoryForFloatInAddresses(DWORD pid, std::vector<LPCV
     return foundAddresses;
 }
 
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-namespace py = pybind11;
+int main() {
+    DWORD pid = 34604;
+    float targetValue = 12.680f;
+    float tolerance = 0.0005f;
+    std::vector<int> addresses = SearchMemoryForFloat(pid, targetValue, tolerance);
+    // for (auto address : addresses) {
+    //     std::cout << "Found float at address: " << std::hex << address << std::endl;
+    // }
 
-PYBIND11_MODULE(memory_search, m) {
-    m.doc() = "Memory module for searching for floats in memory";
-    m.def("search_memory_for_float", &SearchMemoryForFloat);
-    m.def("search_memory_for_float_in_addresses", &SearchMemoryForFloatInAddresses);
+    std::cout << "Press Enter to continue...";
+    std::cin.get();
+
+    targetValue = -2.3425f;
+    tolerance = 0.0005f;
+    std::vector<int> addresses2 = SearchMemoryForFloatInAddresses(pid, addresses, targetValue, tolerance);
+    for (auto address : addresses2) {
+        std::cout << "Found float at address: " << std::hex << address << std::endl;
+    }
+
+    return 0;
+
 }
+
+// #include <pybind11/pybind11.h>
+// #include <pybind11/stl.h>
+// namespace py = pybind11;
+
+// PYBIND11_MODULE(memory_search, m) {
+//     m.doc() = "Memory module for searching for floats in memory";
+//     m.def("search_memory_for_float", &SearchMemoryForFloat);
+//     m.def("search_memory_for_float_in_addresses", &SearchMemoryForFloatInAddresses);
+// }
