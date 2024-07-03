cae536acf240d546773367d453599441b74397c4
Campion <ryanchien2468@gmail.com>
2024-07-03 11:53:14 -0700

    reduce amount of debugging

--- utils/control.py
+++ utils/control.py
@@ -1,5 +1,6 @@
 import time
 import math
+import keyboard
 import vgamepad as vg
 import logging
 from utils import memory
@@ -10,45 +11,61 @@ class Control:
         self.logger = logger
         self.gamepad = vg.VX360Gamepad()
 
-    def wake_up(self):
-        self.logger.debug(f"Waking up controller")
-        self.gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_UP)
-        self.gamepad.update()
-        time.sleep(0.1)
-        self.reset()
-
     def reset(self):
         self.gamepad.reset()
         self.gamepad.update()
+        keyboard.release("space")
+
+    def reset_look(self):
+        self.gamepad.right_joystick_float(x_value_float=0, y_value_float=0)
+        self.gamepad.update()
+
+    def reset_move(self):
+        self.gamepad.left_joystick_float(x_value_float=0, y_value_float=0)
+        self.gamepad.update()
 
     def look_down(self, value):
-        self.logger.debug(f"Looking down by {value}")
         self.gamepad.right_joystick_float(x_value_float=0, y_value_float=-value)
         self.gamepad.update()
 
     def look_up(self, value):
-        self.logger.debug(f"Looking up by {value}")
         self.gamepad.right_joystick_float(x_value_float=0, y_value_float=value)
         self.gamepad.update()
 
     def look_left(self, value):
-        self.logger.debug(f"Looking left by {value}")
         self.gamepad.right_joystick_float(x_value_float=-value, y_value_float=0)
         self.gamepad.update()
 
     def look_right(self, value):
-        self.logger.debug(f"Looking right by {value}")
         self.gamepad.right_joystick_float(x_value_float=value, y_value_float=0)
         self.gamepad.update()
 
     def move_forward(self, value):
-        self.logger.debug(f"Moving forward by {value}")
         self.gamepad.left_joystick_float(x_value_float=0, y_value_float=value)
         self.gamepad.update()
 
     # def move_backward(self, value):
     #     self.gamepad.left_joystick_float(x_value_float=0, y_value_float=-value)
     #     self.gamepad.update()
+
+    def jump(self):
+        self.gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+        self.gamepad.update()
+        time.sleep(0.01)
+        self.gamepad.release_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+        self.gamepad.update()
+
+    def zoom_in(self, duration=2):
+        self.logger.debug("Zooming in")
+        keyboard.press("i")
+        time.sleep(duration)
+        keyboard.release("i")
+
+    def zoom_out(self, duration=2):
+        self.logger.debug("Zooming out")
+        keyboard.press("o")
+        time.sleep(duration)
+        keyboard.release("o")
 
     def calculate_degree_difference(self, current_degree, target_degree):
         diff = target_degree - current_degree
@@ -58,22 +75,19 @@ class Control:
             diff -= 360
         return diff
 
-    def turn_towards(self, pid, y_addrs, degree, tolerance):
-        self.logger.debug(f"Turning towards {degree}")
+    def turn_towards_yaw(self, pid, y_addrs, degree, tolerance):
         while True:
             rot = memory.get_current_rot(pid, y_addrs)[1]
             diff = self.calculate_degree_difference(rot, degree)
             if abs(diff) < tolerance:
                 break
-            amount = abs(diff/180)+0.2
+            amount = min(abs(diff/90)+0.2, 1)
             if diff > 0:
-                self.look_right(amount if amount < 1 else 1)
-                time.sleep(0.25)
-                self.reset()
+                self.look_right(amount)
             else:
-                self.look_left(amount if amount < 1 else 1)
-                time.sleep(0.25)
-                self.reset()
+                self.look_left(amount)
+            time.sleep(0.1)
+            self.reset_look()
 
     def calculate_degree_pos(self, x1, z1, x2, z2):
         degrees = math.degrees(math.atan2(z2 - z1, x2 - x1))
@@ -84,22 +98,25 @@ class Control:
     def calculate_distance(self, x1, z1, x2, z2):
         return math.sqrt((x2 - x1) ** 2 + (z2 - z1) ** 2)
 
-    def go_to_pos(self, pid, y_addrs, final_x, final_z, tolerance):
+    def go_to_pos(self, pid, y_addrs, final_x, final_z, tolerance, turn_tolerance=5, jump=False):
         current_pos = memory.get_current_pos(pid, y_addrs)
         init_distance = self.calculate_distance(current_pos[0], current_pos[2], final_x, final_z)
         self.logger.debug(f"Going to {final_x}, {final_z} from {current_pos[0]}, {current_pos[2]}")
         self.logger.debug(f"Distance to target: {init_distance}")
+        final_rot = self.calculate_degree_pos(current_pos[0], current_pos[2], final_x, final_z)
+        self.turn_towards_yaw(pid, y_addrs, final_rot, 5)
         while True:
             current_x = current_pos[0]
             current_z = current_pos[2]
             if abs(current_x - final_x) < tolerance and abs(current_z - final_z) < tolerance:
                 break
             final_rot = self.calculate_degree_pos(current_x, current_z, final_x, final_z)
-            self.turn_towards(pid, y_addrs, final_rot, 5)
+            self.turn_towards_yaw(pid, y_addrs, final_rot, turn_tolerance)
             distance = self.calculate_distance(current_x, current_z, final_x, final_z)
-            self.logger.debug(f"Distance to target: {distance}")
-            amount = abs(distance/init_distance)+0.4
+            amount = abs(distance/(init_distance/2))+0.4
+            if jump:
+                self.jump()
             self.move_forward(amount if amount < 1 else 1)
-            time.sleep(0.25)
-            self.reset()
+            time.sleep(0.1)
+            self.reset_move()
             current_pos = memory.get_current_info(pid, y_addrs)
