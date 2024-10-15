313729a89ca9700127394b32a82307837542f7a8
Campion <ryanchien2468@gmail.com>
2024-10-15 15:42:28 -0700

    cache config to prevent multiple reads and multiple objects

--- config_loader.py
+++ config_loader.py
@@ -2,10 +2,16 @@ import sys
 import types
 
 
+_config = None
+
+
 def load_config():
-    if getattr(sys, 'frozen', False):
-        config = types.ModuleType('config')
-        exec(open("config.py").read(), config.__dict__)
-    else:
-        import config
-    return config
+    global _config
+    if _config is None:
+        if getattr(sys, 'frozen', False):
+            _config = types.ModuleType('config')
+            exec(open("config.py").read(), _config.__dict__)
+        else:
+            import config
+            _config = config
+    return _config
